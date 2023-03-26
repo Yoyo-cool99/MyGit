78f8f843500c96161a63d66a69a3ea5052a04ad8
Yoyo_ <50907124+Yoyo-cool99@users.noreply.github.com>
2023-03-26 14:19:48 +0800

    Update hello.java
    pull-test

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,5 +3,6 @@ public class hello {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("pull-test");
     }
 }
